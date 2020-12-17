b3ca13d440d02863ad9da92403b904933a8bc0cc
Brigham Andersen <brighamband@gmail.com>
2020-12-16 18:27:20 -0700

    Updates

---
+++ precache-manifest.7bb201d0a0d47d327ed45968e3485727.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a9607af9cb8a037cfa4bc075e61da33",
+    "url": "/material-todo/index.html"
+  },
+  {
+    "revision": "3d8e79a52f1157d6f646",
+    "url": "/material-todo/static/css/main.8b81a09d.chunk.css"
+  },
+  {
+    "revision": "06da4832a1a7844b13ee",
+    "url": "/material-todo/static/js/2.5e0eca2a.chunk.js"
+  },
+  {
+    "revision": "8da9ff4ecc7440c09f64cd8625501c41",
+    "url": "/material-todo/static/js/2.5e0eca2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d8e79a52f1157d6f646",
+    "url": "/material-todo/static/js/main.a8ac6f5d.chunk.js"
+  },
+  {
+    "revision": "2095bc0068d7150e481d",
+    "url": "/material-todo/static/js/runtime-main.82b36a0e.js"
+  }
+]);
